a92968d3e99102876de61543409de99d208e2eea
Juan Cassiano <juancassiano@hotmail.com>
2022-12-08 16:21:40 -0300

    "Create Controller"

---
+++ src/main/java/me/dio/diogameawards/controller/GameRestController.java
@@ -0,0 +1,45 @@
+package me.dio.diogameawards.controller;
+
+import me.dio.diogameawards.domain.model.Game;
+import me.dio.diogameawards.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GameRestController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping("games")
+    public ResponseEntity<List<Game>> findAll(){
+        List<Game> games = gameService.findAll();
+        return ResponseEntity.ok(games);
+    }
+
+    @GetMapping("games/{id}")
+    public ResponseEntity<Game> findById(@PathVariable Long id){
+        Game game = gameService.findById(id);
+        return ResponseEntity.ok(game);
+    }
+
+    @PostMapping("games")
+    public ResponseEntity<Game> insert(@RequestBody Game game){
+        gameService.insert(game);
+        return ResponseEntity.ok(game);
+    }
+    @PutMapping("games/{id}")
+    public ResponseEntity<Game> insert(@PathVariable Long id, @RequestBody Game game){
+        gameService.update(id, game);
+        return ResponseEntity.ok(game);
+    }
+
+    @DeleteMapping("games/{id}")
+    public ResponseEntity<Game> insert(@PathVariable Long id){
+        gameService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
